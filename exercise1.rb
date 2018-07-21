ad30ede1c2ff996df5af8a8426fb9eb446ef5e6b
Billy Davidson <billy.davids52@gmail.com>
2018-07-21 19:55:35 -0400

    exercise1 complete

---
+++ exercise1.rb
@@ -0,0 +1,24 @@
+colours = ["red", "yellow", "pink", "green"]
+family_ages = [29, 31, 10, 1]
+coin_toss = ["no", "yes", "no", "yes", "yes"]
+artists = ["Bruce Springsteen", "Tom Waits", "Pokey Lafarge"]
+colours = [:red, :yellow, :pink, :green, :purple]
+
+
+words = {gestalt: "an organized whole that is perceived as more than the sum of its parts", zenith: "an imaginary point directly above a particular location", cromulent: "not a word, but used to mean correct, relevant, or acceptable"}
+#
+cities = {toronto: "3000000", new_york: "10000000", tokyo: "20000000"}
+#
+movies = {The_Neon_Demon: "2016", Bill_and_Teds_Excellent_Adventure: "1988", The_Life_Aquatic: "2006", The_Mask: "1999"}
+#
+# shows = {:2001 => "The Sopranos" :2000 => "Six Feet Under"}
+
+# shows = {sopranos: "2001", six_feet_under: "2000"}
+
+family_ages_hash = {caitlin: "31", bear: "1", peat: "10", me: "29"}
+
+p coin_toss
+p colours[0]
+p family_ages.sort
+p family_ages << 0
+p movies[:The_Neon_Demon]
