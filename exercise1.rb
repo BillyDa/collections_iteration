dafe3067f6754b95d969aa3a1f15d12a125facd0
Billy Davidson <billy.davids52@gmail.com>
2018-07-21 21:18:06 -0400

    through 4

--- exercise1.rb
+++ exercise1.rb
@@ -12,14 +12,34 @@ movies = {The_Neon_Demon: "2016", Bill_and_Teds_Excellent_Adventure: "1988", The
 
 family_ages_hash = {caitlin: "31", bear: "1", peat: "10", me: "29"}
 
-p artists[0,2]
+#EX4
+cities[:toronto] = "3500000"
+p cities
 
-movies.each do |film, release|
-  puts "#{film} came out in #{release}"
+p artists.delete_at(1)
+p artists
+p artists << "Tom Waits"
+
+yes = coin_toss.count('yes')
+p "Heads were flipped #{yes} times out of 5"
+
+p family_ages.max
+
+family_ages.each do |young|
+  if young < 30
+    puts young
+  end
 end
-"
-p family_ages.sort.reverse
-
+#EX3
+# p artists[0,2]
+# movies.each do |film, release|
+#   puts "#{film} came out in #{release}"
+# end
+#
+# p family_ages.sort.reverse
+# movies[:Beauty_and_the_Beast] = "1991, 2017"
+# p movies
+#EX2
 # p colours[3]
 # cities[:halifax] = "1000000"
 # p cities
@@ -29,7 +49,8 @@ p family_ages.sort.reverse
 # p "I think #{artists[0]} is great"
 # p "I think #{artists[1]} is sexy"
 # p "I think #{artists[2]} is cool"
-# p coin_toss
+# EX1
+#p coin_toss
 # p colours[0]
 # p family_ages.sort
 # p family_ages << 0
