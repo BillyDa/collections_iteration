34107e4a89b7a979c7bb323617733c2a211468d7
Billy Davidson <billy.davids52@gmail.com>
2018-07-21 20:17:08 -0400

    ex2 done right

--- exercise1.rb
+++ exercise1.rb
@@ -12,15 +12,23 @@ movies = {The_Neon_Demon: "2016", Bill_and_Teds_Excellent_Adventure: "1988", The
 
 family_ages_hash = {caitlin: "31", bear: "1", peat: "10", me: "29"}
 
-p colours[3]
-cities[:halifax] = "1000000"
-p cities
-p coin_toss.reverse
-p cities[:tokyo]
-artist = "is awesome and sexy"
-p "I think #{artists[0]} is great"
-p "I think #{artists[1]} is sexy"
-p "I think #{artists[2]} is cool"
+p artists[0,2]
+
+movies.each do |film, release|
+  puts "#{film} came out in #{release}"
+end
+"
+p family_ages.sort.reverse
+
+# p colours[3]
+# cities[:halifax] = "1000000"
+# p cities
+# p coin_toss.reverse
+# p cities[:tokyo]
+# artist = "is awesome and sexy"
+# p "I think #{artists[0]} is great"
+# p "I think #{artists[1]} is sexy"
+# p "I think #{artists[2]} is cool"
 # p coin_toss
 # p colours[0]
 # p family_ages.sort
